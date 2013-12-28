a745b883cf27e07d26b12e7250427e703ae50009
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-12-28 22:24:00 +0100

    created the raumfeld class

--- raumfeld.py
+++ raumfeld.py
@@ -2,7 +2,7 @@ import socket
 from pysimplesoap.client import SoapClient
 
 
-def discover(service, timeout=2, retries=1):
+def discover(service='ssdp:all', timeout=2, retries=1):
     group = ("239.255.255.250", 1900)
     message = "\r\n".join([
         'M-SEARCH * HTTP/1.1',
@@ -12,8 +12,7 @@ def discover(service, timeout=2, retries=1):
     socket.setdefaulttimeout(timeout)
     responses = {}
     for _ in range(retries):
-        sock = socket.socket(socket.AF_INET,
-                             socket.SOCK_DGRAM,
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
                              socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
@@ -28,27 +27,41 @@ def discover(service, timeout=2, retries=1):
     return responses
 
 
-# devices = discover('ssdp:urn:schemas-upnp-org:device:MediaRenderer:1')
-# for device in devices:
-#     print device
+class RaumfeldDevice(object):
 
-rendering_control = SoapClient(
-    location="http://192.168.178.37:42342/RenderingService/Control",
-    action="urn:upnp-org:serviceId:RenderingControl#",
-    namespace="http://schemas.xmlsoap.org/soap/envelope/",
-    soap_ns='soap', ns='s', exceptions=True)
+    def __init__(self, ip, port):
+        self.address = 'http://%s:%s' % (ip, port)
+        self.rendering_control = SoapClient(
+            location='%s/RenderingService/Control' % self.address,
+            action='urn:upnp-org:serviceId:RenderingControl#',
+            namespace='http://schemas.xmlsoap.org/soap/envelope/',
+            soap_ns='soap', ns='s', exceptions=True)
 
-av_transport = SoapClient(
-    location="http://192.168.178.37:42342/TransportService/Control",
-    action="urn:schemas-upnp-org:service:AVTransport:1#",
-    namespace="http://schemas.xmlsoap.org/soap/envelope/",
-    soap_ns='soap', ns='s', exceptions=True)
+        self.av_transport = SoapClient(
+            location='%s/TransportService/Control' % self.address,
+            action='urn:schemas-upnp-org:service:AVTransport:1#',
+            namespace='http://schemas.xmlsoap.org/soap/envelope/',
+            soap_ns='soap', ns='s', exceptions=True)
 
-# response = av_transport.Play(InstanceID=1, Speed=2)
-response = av_transport.Pause(InstanceID=1)
-# response = av_transport.Play(InstanceID=1)
-# response = rendering_control.SetVolume(InstanceID=1, DesiredVolume=0)
-# response = rendering_control.SetMute(InstanceID=1, DesiredMute=0)
+    def play(self):
+        self.av_transport.Play(InstanceID=1, Speed=2)
 
-# response = rendering_control.GetVolume(InstanceID=1)
-# print response.CurrentVolume
+    def pause(self):
+        self.av_transport.Pause(InstanceID=1)
+
+    def set_volume(self, volume):
+        self.rendering_control.SetVolume(InstanceID=1, DesiredVolume=volume)
+
+    def get_volume(self):
+        response = self.rendering_control.GetVolume(InstanceID=1)
+        return response.CurrentVolume
+
+    def set_mute(self, mute):
+        self.rendering_control.SetMute(InstanceID=1, DesiredMute=mute)
+
+
+if __name__ == '__main__':
+    devices = discover('ssdp:urn:schemas-upnp-org:device:MediaRenderer:1')
+    print devices
+    box = RaumfeldDevice(ip='192.168.178.37', port=42342)
+    box.pause()
