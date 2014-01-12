7474161d959a8e0883a4058cc7c511348d806282
Thomas Feldmann <feldmann.thomas@googlemail.com>
2014-01-12 15:16:49 +0100

    Search for devices works again

--- desktop_application/mainwindow.py
+++ desktop_application/mainwindow.py
@@ -1,5 +1,4 @@
-import time
-from PySide.QtCore import QObject, QThread, Signal, Slot
+from PySide.QtCore import QThread, Signal, Slot
 from PySide.QtGui import QMainWindow
 import raumfeld
 from .mainwindow_ui import Ui_MainWindow as Ui
@@ -7,16 +6,14 @@ from .mainwindow_ui import Ui_MainWindow as Ui
 __version__ = '0.2'
 
 
-class RaumfeldWrapper(QObject):
+class RaumfeldSearchThread(QThread):
 
     devices_found = Signal(list)
 
     def __init__(self, parent=None):
-        #super(raumfeld.RaumfeldDevice, self).__init__(parent)
-        super(RaumfeldWrapper, self).__init__(parent)
+        super(RaumfeldSearchThread, self).__init__(parent)
 
-    @Slot()
-    def discover(self):
+    def run(self):
         devices = raumfeld.discover()
         self.devices_found.emit(devices)
 
@@ -42,20 +39,14 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Raumfeld v%s' % __version__)
         self.setEnabled(False)
 
-        # create worker thread for device communication
-        self.thread = QThread()
-        self.device = RaumfeldWrapper()
-        self.device.devices_found.connect(self.devices_found)
-        self.search_for_devices.connect(self.device.discover)
-
-        self.device.moveToThread(self.thread)
-        self.thread.start()
-
-        self.search_for_devices.emit()
-
         # interconnect sliders
         self.ui.dialVolume.valueChanged.connect(self.ui.sliderVolume.setValue)
         self.ui.sliderVolume.valueChanged.connect(self.ui.dialVolume.setValue)
+
+        # create worker thread for device communication
+        self.search_thread = RaumfeldSearchThread()
+        self.search_thread.devices_found.connect(self.devices_found)
+        self.search_thread.start()
 
     @Slot(int)
     def on_dialVolume_valueChanged(self, value):
@@ -67,7 +58,11 @@ class MainWindow(QMainWindow):
 
     @Slot(list)
     def devices_found(self, devices):
-        print(devices)
+        self.device = devices[0]
+        self.ui.sliderVolume.setValue(self.device.get_volume())
+        self.ui.lblConnection.setText(self.device.friendly_name)
+
+        self.setEnabled(True)
 
     def closeEvent(self, event):
-        self.thread.quit()
+        self.search_thread.quit()
